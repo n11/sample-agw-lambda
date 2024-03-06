3ea14ef1c276ac54572e136d8ecfaac4688b6884
Noritsugu Endo <noen@amazon.co.jp>
2024-03-07 08:52:40 +0900

    Add sample

--- lib/sample-agw-lambda-stack.ts
+++ lib/sample-agw-lambda-stack.ts
@@ -1,19 +1,27 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class SampleAgwLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'SampleAgwLambdaQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // define an AWS Lambda resource
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_20_X, // execution environment
+      code: Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler', // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'SampleAgwLambdaTopic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // define an API Gateway REST API resource backed by our "hello" function
+    const gateway = new LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler,
+    });
   }
 }
